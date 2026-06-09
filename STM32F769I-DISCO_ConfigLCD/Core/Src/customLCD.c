5c5d8e840f5f2f0fdce7d3d327f70ff46c0490fe
Debasish Das <debasishtech@gmail.com>
2026-06-09 15:08:59 +0530

    Reposition void MPU_SDRAM_Config()

--- STM32F769I-DISCO_ConfigLCD/Core/Src/customLCD.c
+++ STM32F769I-DISCO_ConfigLCD/Core/Src/customLCD.c
@@ -134,3 +134,27 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 	}
 }
 
+
+
+void MPU_SDRAM_Config()
+	{
+		MPU_Region_InitTypeDef MPU_InitStruct = { 0 };
+
+		HAL_MPU_Disable();
+
+		/* SDRAM: 0xC0000000, 8MB */
+		MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+		MPU_InitStruct.BaseAddress = 0xC0000000;
+		MPU_InitStruct.Size = MPU_REGION_SIZE_8MB;
+		MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+		MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+		MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+		MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+		MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+		MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+		MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+		HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+		HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+	}
